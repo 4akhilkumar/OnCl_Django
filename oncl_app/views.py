c65c18dcc8ed1e2952f679c99eef9d9999e80d1a
Sai Akhil Kumar Reddy Nallamilli <190031153@kluniversity.in>
2021-02-20 19:32:34 +0530

    Initial Commit

---
+++ oncl_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from .models import *
+from .forms import CreateUserForm
+
+def home_page(request):
+    return render(request,'oncl_app/home.html')
+
+def register_page(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+
+		context = {'form':form}
+		return render(request, 'oncl_app/register.html', context)
+
+def login_page(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'oncl_app/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
