0e154d70a7467056cebedb75b73d1ea763e69eaf
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-03-30 16:40:16 +0530

    Page Not Found Code Lines

--- oncl_app/views.py
+++ oncl_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.http import HttpResponse
+from django import template
 
 # Create your views here.
 from .models import *
@@ -50,3 +53,20 @@ def login_page(request):
 def logoutUser(request):
 	logout(request)
 	return redirect('login')
+
+def pages(request):
+    context = {}
+    try:
+        load_template = request.path.split('/')[-1]
+        context['segment'] = load_template
+        
+        html_template = loader.get_template( load_template )
+        return HttpResponse(html_template.render(context, request))
+        
+    except template.TemplateDoesNotExist:
+        html_template = loader.get_template( 'oncl_app/page-404.html' )
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template( 'oncl_app/page-500.html' )
+        return HttpResponse(html_template.render(context, request))
