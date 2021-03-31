ef7fd824be50cb7d1ac0b34664af1ba37ef262b5
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-03-31 18:35:53 +0530

    Update settings.py

--- oncl_project/settings.py
+++ oncl_project/settings.py
@@ -153,4 +153,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '4projtest@gmail.com'
-EMAIL_HOST_PASSWORD = 'TESTINGgmail@20'
+EMAIL_HOST_PASSWORD = 'gsfwjlthbinuvkda'
