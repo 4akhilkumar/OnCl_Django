8b37d9ff411f31531c6e0f10f591ae14a4522c28
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-04-01 23:12:32 +0530

    Message Tags

--- oncl_project/settings.py
+++ oncl_project/settings.py
@@ -154,3 +154,15 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '4projtest@gmail.com'
 EMAIL_HOST_PASSWORD = 'gsfwjlthbinuvkda'
+
+from django.contrib.messages import constants as messages
+from django.contrib.messages import constants as message_constants
+MESSAGE_LEVEL = message_constants.DEBUG
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
