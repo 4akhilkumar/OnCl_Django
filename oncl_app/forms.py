00f7ea70cf1a1b9fa39c72b4ae84d391532d31d9
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-04-14 22:48:16 +0530

    PositionForm class is created

--- oncl_app/forms.py
+++ oncl_app/forms.py
@@ -7,3 +7,6 @@ class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class PositionForm(forms.Form):
+    position = forms.CharField()
