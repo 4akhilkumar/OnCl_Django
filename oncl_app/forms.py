0966d50deebf4307c897479742ef2daea8e98576
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-04-15 22:33:29 +0530

    ContactForm class is created

--- oncl_app/forms.py
+++ oncl_app/forms.py
@@ -10,3 +10,9 @@ class CreateUserForm(UserCreationForm):
 
 class PositionForm(forms.Form):
     position = forms.CharField()
+
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    from_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
