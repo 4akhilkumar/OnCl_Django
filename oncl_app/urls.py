1eed969463f442948ff804c116025a3a0441e50c
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-04-16 15:19:27 +0530

    template paths are added

--- oncl_app/urls.py
+++ oncl_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, TaskReorder
+from .views import TaskList, TaskCreate, TaskUpdate, DeleteView, TaskReorder
 from . import views
 
 urlpatterns = [
@@ -9,30 +9,35 @@ urlpatterns = [
     path('register/', views.register_page, name = 'register'),
 	path('logout/', views.logoutUser, name="logout"),
     path('reset_password/',
-        auth_views.PasswordResetView.as_view(template_name="oncl_app/password_reset.html"),
+        auth_views.PasswordResetView.as_view(template_name="oncl_app/password_reset/password_reset.html"),
         name="reset_password"),
-    
     path('reset_password_sent/',
-        auth_views.PasswordResetDoneView.as_view(template_name="oncl_app/password_reset_sent.html"),
+        auth_views.PasswordResetDoneView.as_view(template_name="oncl_app/password_reset/password_reset_sent.html"),
         name="password_reset_done"),
-    
     path('reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(template_name="oncl_app/password_reset_form.html"),
+        auth_views.PasswordResetConfirmView.as_view(template_name="oncl_app/password_reset/password_reset_form.html"),
         name="password_reset_confirm"),
-
     path('reset_password_complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name="oncl_app/password_reset_complete.html"),
+        auth_views.PasswordResetCompleteView.as_view(template_name="oncl_app/password_reset/password_reset_complete.html"),
         name="password_reset_complete"),
     
-    path('feedback/', views.feedback_page, name = 'feedback'),
+    path('feedback/', views.feedback_page, name='feedback'),
     path('success/', views.successView, name='success'),
 
-    path('audio/', views.audio_page, name = 'audio'),
-    path('task/', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-    path('task-create/', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
-    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+    path('audio/', views.audio_page, name = 'audio'), 
+
+    path('tasks/', 
+        TaskList.as_view(template_name="oncl_app/task/task_list.html"),
+        name='tasks'),
+    path('task-create/',
+        TaskCreate.as_view(template_name="oncl_app/task/task_form.html"),
+        name='task-create'),
+    path('task-update/<int:pk>/',
+        TaskUpdate.as_view(template_name="oncl_app/task/task_form.html"),
+        name='task-update'),
+    path('task-delete/<int:pk>/',
+        DeleteView.as_view(template_name="oncl_app/task/task_confirm_delete.html"),
+        name='task-delete'),
     path('task-reorder/', TaskReorder.as_view(), name='task-reorder'),
 
     path('dashboard/', views.dashboard_page, name = 'dashboard'),
