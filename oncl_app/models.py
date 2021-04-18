f9335a7ba0f3a9a841028c2caa0f3af28a3c084d
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-04-19 01:51:35 +0530

    Models for PCS_Cloud are created

--- oncl_app/models.py
+++ oncl_app/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PCS_Cloud(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    course_name = models.CharField(max_length=200)
+    topics = models.TextField(max_length=200, null=True, blank=True)
+    url = models.TextField(max_length=200, null=True, blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+    faculty_name = models.CharField(max_length=200)
+    faculty_email = models.CharField(max_length=200)
+    faculty_phno = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.course_name
+    
+    class Meta:
+        order_with_respect_to = 'user'
+
 class Task(models.Model):
-    user = models.ForeignKey(
-    User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
