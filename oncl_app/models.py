cd4577d6a84fbfdfdfa83348ed77ec3be36fc64f
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-05-12 14:34:14 +0530

    students model is created

--- oncl_app/models.py
+++ oncl_app/models.py
@@ -66,3 +66,15 @@ class Staffs(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Students(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    gender = models.CharField(max_length=50, default="")
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.user.username
