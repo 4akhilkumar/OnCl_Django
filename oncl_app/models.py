6323b31825939a09e0b42e625bdece451fe94e3d
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-05-12 21:01:13 +0530

    attendance model deleted

--- oncl_app/models.py
+++ oncl_app/models.py
@@ -70,6 +70,7 @@ class Staffs(models.Model):
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete = models.CASCADE)
+    branch = models.CharField(max_length=255, default="")
     gender = models.CharField(max_length=50, default="")
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -78,24 +79,6 @@ class Students(models.Model):
 
     def __str__(self):
         return self.user.username
-
-class Attendance(models.Model):
-    id = models.AutoField(primary_key=True)
-    subject_id = models.ForeignKey(Subjects, on_delete=models.DO_NOTHING)
-    attendance_date = models.DateField()
-    session_year_id = models.ForeignKey(Semester, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.Manager()
-
-class AttendanceReport(models.Model):
-    id = models.AutoField(primary_key=True)
-    student_id = models.ForeignKey(Students, on_delete=models.DO_NOTHING)
-    attendance_id = models.ForeignKey(Attendance, on_delete=models.CASCADE)
-    status = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = models.Manager()
 
 class LeaveReportStudent(models.Model):
     id = models.AutoField(primary_key=True)
