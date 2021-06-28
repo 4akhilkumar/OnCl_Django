26abcf8ee6e4d4c14695a546cf9a4ae81882554a
Sai Akhil Kumar Reddy Nallamilli <4akhilkumar@gmail.com>
2021-06-28 09:37:04 +0530

    os_details and browser_details fields added

--- oncl_app/models.py
+++ oncl_app/models.py
@@ -151,6 +151,8 @@ class user_login_details(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ip_addr = models.CharField(max_length=100)
+    os_details = models.CharField(max_length=100)
+    browser_details = models.CharField(null=False, blank=False, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
