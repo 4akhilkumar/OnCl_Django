78e1e3850ad2ed4719e1078078d5ba3775d75511
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-07-08 13:49:07 +0530

    New App Key

--- oncl_project/settings.py
+++ oncl_project/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '4projtest@gmail.com'
-EMAIL_HOST_PASSWORD = 'nhevnvnhhfejbzyn'
+EMAIL_HOST_PASSWORD = 'ansgmitfrhboysbh'
 
 from django.contrib.messages import constants as messages
 from django.contrib.messages import constants as message_constants
