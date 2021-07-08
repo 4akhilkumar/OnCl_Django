fa22c08918c7b4bdf9876ee9c1ee7e14f5cec906
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-07-08 12:58:10 +0530

    UUID, content modified, Foreign Key: PCS_Cloud, E_Books

--- oncl_app/models.py
+++ oncl_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Note: If migrations didn't detected then use this command -> py manage.py makemigrations app_name
 
@@ -18,8 +19,8 @@ BRANCH_CHOICES = [
 ]
 
 class Branches(models.Model):
-    id = models.AutoField(primary_key=True)
-    branch = models.CharField(max_length=50, choices = BRANCH_CHOICES, default=1)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    branch = models.CharField(max_length=50, unique=True, choices = BRANCH_CHOICES, default=1)
     objects = models.Manager()
 
     def __str__(self):
@@ -30,7 +31,7 @@ class Semester(models.Model):
         ('EVEN','EVEN'),
         ('ODD','ODD'),
     )
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     semester_mode = models.CharField(max_length=4, choices=SEM_MODE)
     branch = models.CharField(max_length=50, choices = BRANCH_CHOICES, default=1)
     semester_start_year = models.DateField()
@@ -41,11 +42,11 @@ class Semester(models.Model):
         return '%s %s %s-%s' % (self.branch, self.semester_mode, self.semester_start_year.year, self.semester_end_year.year)
 
 class Subjects(models.Model):
-    id = models.AutoField(primary_key=True)
-    subject_name = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subject_name = models.CharField(max_length=50, unique=True,)
     branch = models.CharField(max_length=50, choices = BRANCH_CHOICES, default=1)
-    semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, blank=True, null=True, default=1)
-    staff_id = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    semester = models.ManyToManyField(Semester)
+    staff_id = models.ManyToManyField(User)
     desc = models.TextField(blank=True, null=True)
     objects = models.Manager()
 
@@ -78,15 +79,15 @@ MOTHER_TOUNGE_CHOICES = [
 ]
 
 class Staffs(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, unique=True, on_delete = models.CASCADE)
     gender = models.CharField(max_length=14, choices = GENDER_CHOICES, default=1)
     father_name = models.CharField(max_length=100, default="Not Provided")
     father_occ = models.CharField(max_length=100, default="Not Provided")
     father_phone = models.CharField(max_length=10, default="9999999999")
     mother_name = models.CharField(max_length=100, default="Not Provided")
     mother_tounge = models.CharField(max_length=50, choices = MOTHER_TOUNGE_CHOICES, default=1)
-    dob = models.DateField(default='2000-01-01')
+    dob = models.DateField(default='1975-01-01')
     blood_group = models.CharField(max_length=18, choices = BLOOD_GROUP_CHOICES, default=1)
     phone = models.CharField(max_length=10, default="9999999999")
     dno_sn = models.CharField(max_length=100, default="A-BCD, On Earth")
@@ -109,15 +110,15 @@ class Staffs(models.Model):
         ordering = ["user__username"]
 
 class Students(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, unique=True, on_delete = models.CASCADE)
     gender = models.CharField(max_length=14, choices = GENDER_CHOICES, default=1)
     father_name = models.CharField(max_length=100, default="Not Provided")
     father_occ = models.CharField(max_length=100, default="Not Provided")
     father_phone = models.CharField(max_length=10, default="9999999999")
     mother_name = models.CharField(max_length=100, default="Not Provided")
     mother_tounge = models.CharField(max_length=50, choices = MOTHER_TOUNGE_CHOICES, default=1)
-    dob = models.DateField(default='2000-01-01')
+    dob = models.DateField(default='1998-01-01')
     blood_group = models.CharField(max_length=18, choices = BLOOD_GROUP_CHOICES, default=1)
     phone = models.CharField(max_length=10, default="9999999999")
     dno_sn = models.CharField(max_length=100, default="A-BCD, On Earth")
@@ -138,7 +139,7 @@ class Students(models.Model):
         ordering = ["user__username"]
     
 class Student_Social_Profile(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     linkedin = models.CharField(max_length=100, default="no_linkedin")
     github = models.CharField(max_length=100, default="no_github")
@@ -147,7 +148,7 @@ class Student_Social_Profile(models.Model):
         return '%s %s %s' % (self.user, self.linkedin, self.github)
 
 class Staff_Social_Profile(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     linkedin = models.CharField(max_length=100, default="no_linkedin")
     github = models.CharField(max_length=100, default="no_github")
@@ -159,18 +160,28 @@ class Staff_Social_Profile(models.Model):
     def __str__(self):
         return '%s %s %s' % (self.user, self.linkedin, self.gscholar)
 
+FLOORS = [
+    ("","Select Floor"),
+    ("1st","1st"),
+    ("2nd","2nd"),
+    ("3rd","3rd"),
+    ("4th","4th"),
+    ("5th","5th"),
+    ("6th","6th"),
+]
+
 class Sections(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     section_name = models.CharField(max_length=100)
     room_no = models.CharField(max_length=100)
-    floor_no = models.CharField(max_length=100)
+    floor_no = models.CharField(max_length=4, choices = FLOORS)
     block_name = models.CharField(max_length=100)
 
     def __str__(self):
         return 'Section %s, %s %s' % (self.section_name, self.room_no, self.block_name)
 
 class Student_Sem_Reg(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -178,7 +189,7 @@ class Student_Sem_Reg(models.Model):
         return '%s %s' % (self.user, self.semester)
 
 class Student_Course_Reg(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     subject = models.ForeignKey(Subjects, on_delete=models.SET_NULL, blank=True, null=True)
     section = models.ForeignKey(Sections, on_delete=models.SET_NULL, blank=True, null=True)
@@ -187,19 +198,19 @@ class Student_Course_Reg(models.Model):
         return '%s %s' % (self.user, self.subject)
 
 class user_login_details(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-    ip_addr = models.CharField(max_length=100)
+    user_ip_address = models.CharField(max_length=100)
     os_details = models.CharField(max_length=100)
     browser_details = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
     def __str__(self):
-        return '%s - %s' % (self.user.username, self.ip_addr)
+        return '%s - %s' % (self.user, self.user_ip_address)
 
 class LeaveReportStudent(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student_id = models.ForeignKey(Students, on_delete=models.SET_NULL, blank=True, null=True)
     leave_date = models.CharField(max_length=50)
     leave_message = models.TextField()
@@ -212,7 +223,7 @@ class LeaveReportStudent(models.Model):
         return '%s %s' % (self.student_id, self.leave_date)
 
 class LeaveReportStaff(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     staff_id = models.ForeignKey(Staffs, on_delete=models.SET_NULL, blank=True, null=True)
     leave_date = models.CharField(max_length=50)
     leave_message = models.TextField()
@@ -229,8 +240,7 @@ class Announcements_news(models.Model):
     sub_an = models.CharField(max_length=100, default="")
     what_an = models.TextField()
     an_image = models.FileField(null=True, blank=True, default='False',upload_to='announcements/')
-    an_user = models.CharField(max_length=100, default="")
-    an_by = models.CharField(max_length=100, default="")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
@@ -239,44 +249,43 @@ class Announcements_news(models.Model):
         return self.sub_an
 
 class E_Books(models.Model):
-    id = models.AutoField(primary_key=True)
-    book_id = models.CharField(max_length=100, default="NA")
-    book_name = models.CharField(max_length=50, default="NA")
-    book_author = models.CharField(max_length=50, default="NA")
-    book_author_uid = models.CharField(max_length=50, default="")
-    book_pub_date = models.DateField()
-    book_desc = models.TextField(max_length=255, default="NA")
-    book_tag1 = models.CharField(max_length=20, default="NA")
-    book_tag2 = models.CharField(max_length=20, default="NA")
-    book_tag3 = models.CharField(max_length=20, default="NA")
-    book_tag4 = models.CharField(max_length=20, default="NA")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    book_id = models.CharField(max_length=13, default="B079ZZLRRL")
+    book_name = models.CharField(max_length=100, default="Django for Beginners: Build websites with Python and Django")
+    book_author = models.CharField(max_length=100, default="William S. Vincent")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    book_pub_date = models.DateField(default="2018-03-07")
+    book_desc = models.TextField(max_length=255, default="Django for Beginners is a project-based introduction to Django, the popular Python-based web framework. Suitable for total beginners who have never built a website before as well as professional programmers looking for a fast-paced guide to modern web development and Django fundamentals.")
+    book_tag1 = models.CharField(max_length=20, default="William S. Vincent")
+    book_tag2 = models.CharField(max_length=20, default="Django")
+    book_tag3 = models.CharField(max_length=20, default="Python")
+    book_tag4 = models.CharField(max_length=20, default="Web Application")
     book_pic = models.ImageField(null=True, blank=True, default='False',upload_to='books/')
-    book_file = models.FileField(null=True, blank=True, default='False',upload_to='books/')
+    book_file = models.FileField(default='False',upload_to='books/')
 
     def __str__(self):
         return self.book_name
 
 class PCS_Cloud(models.Model):
-    id = models.AutoField(primary_key=True)
-    session_id = models.CharField(max_length=100, default="NA")
-    session_name = models.CharField(max_length=50, default="NA")
-    session_author = models.CharField(max_length=50, default="NA")
-    session_author_uid = models.CharField(max_length=50, default="")
-    session_pub_date = models.DateField()
-    session_desc = models.TextField(max_length=255, default="NA")
-    session_tag1 = models.CharField(max_length=20, default="NA")
-    session_tag2 = models.CharField(max_length=20, default="NA")
-    session_tag3 = models.CharField(max_length=20, default="NA")
-    session_tag4 = models.CharField(max_length=20, default="NA")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    session_ref_no = models.CharField(max_length=13, default="4488")
+    session_name = models.CharField(max_length=50, default="Enterprise Programming")
+    session_desc = models.TextField(max_length=255, default="Java Enterprise edition has changed and evolved a lot over the years. This course is about teaching you what JEE is, and how to use it and become a productive JEE developer. The course focuses on JEE 8 (also known as Jakarta EE).")
+    session_pub_date = models.DateField(default="2021-04-04")
+    user = models.ForeignKey(Staffs, on_delete=models.SET_NULL, blank=True, null=True)
+    session_tag1 = models.CharField(max_length=25, default="Java")
+    session_tag2 = models.CharField(max_length=25, default="Enterprise Programming")
+    session_tag3 = models.CharField(max_length=25, default="Web Services")
+    session_tag4 = models.CharField(max_length=25, default="Testing - Java")
     session_pic = models.ImageField(upload_to='sessions/', null=True, blank=True, default="False")
     session_file = models.FileField(upload_to='sessions/', null=True, blank=True, default="False")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.session_name
+        return '%s %s' % (self.session_name, self.user.user.first_name)
 
 class Exam(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student_id = models.ForeignKey(Students, on_delete=models.SET_NULL, blank=True, null=True)
     exam_sub = models.CharField(max_length=50)
     ans_file = models.FileField(upload_to='answers/')
@@ -285,7 +294,7 @@ class Exam(models.Model):
     objects = models.Manager()
 
 class Exam_ques(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     subject_id = models.ForeignKey(Subjects, on_delete=models.SET_NULL, blank=True, null=True)
     exam_file = models.FileField(upload_to='question/')
     objects = models.Manager()
@@ -301,7 +310,7 @@ class OnlineClassRoom(models.Model):
     objects=models.Manager()
 
 class AttendanceReportStudent(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     student_id = models.IntegerField(default=0)
     branch_id = models.CharField(max_length=50)
     subject = models.CharField(max_length=50)
