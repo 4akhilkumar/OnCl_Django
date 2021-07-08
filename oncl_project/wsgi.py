0be305ffa5453823dd1c085ca791a3527bc40063
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-07-08 12:34:42 +0530

    Changes to seek video during Production in Django

--- oncl_project/wsgi.py
+++ oncl_project/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from static_ranges import Ranges
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oncl_project.settings')
 
-application = get_wsgi_application()
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
